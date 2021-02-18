b73ec522db27f9971c002b78602ca25c37a1614d
maks <makskosar79@gmil.com>
2021-02-18 12:01:58 +0200

    done with GET and POST

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ const http = require("http");
 const url = require('url');
 
 http.createServer((request, response) => {
-    console.log("start server...");
-    console.log(request.method);
-    let urlReq = url.parse(request.url, true);
-    console.log(urlReq.query.test_number_1);
-    if (urlReq.query.test_number_1 === String){
-        response.end(`you write `)
+    if (request.method == 'GET') {
+        console.log("start server...");
+        console.log(request.method);
+        let urlReq = url.parse(request.url, true);
+        console.log(urlReq.query.test);
+        if (urlReq.query.test % 2 == 0) {
+            response.end(`you right `);
+        }
+        response.end("just another number ");
+    } else {
+        //POST
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            console.log(body);
+            response.end('all working');
+        })
     }
-    response.end("just another number ");
-  })
-  .listen(3001);
+})
+    .listen(3001);
